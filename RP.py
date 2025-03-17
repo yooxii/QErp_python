41dfcb1f1e0f070333f3e791043333568b61f9ce
Lucas Li <2385014552@qq.com>
2025-03-17 10:34:51 +0800

    pyside2第一次提交

---
+++ RP.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QGridLayout, QWidget, QFileDialog, QMessageBox
+from RPMainWindow import Ui_MainWindow
+
+class RPMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(RPMainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.action_openfolder.triggered.connect(self.open_folder)
+
+        self.action_quitapp.triggered.connect(self.close)
+
+    def open_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Open Folder", os.path.expanduser("~"))
+        self.lineEdit_folderpath.setText(folder_path)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes | 
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = RPMainWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
